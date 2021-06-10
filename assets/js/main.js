0e3ea296bf5c4739010e9513dbf8de7da6094291
ali-malek-dev <ali.malek.dev@gmail.com>
2021-06-10 13:47:27 +0430

    login form added

--- assets/js/main.js
+++ assets/js/main.js
@@ -262,3 +262,31 @@ function openform(evt, formName) {
   document.getElementById(formName).style.display = "block";
   evt.currentTarget.className += " tab-active";
 }
+
+const modal = document.querySelector(".login-modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelector(".open-modal");
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  console.log("hello");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
